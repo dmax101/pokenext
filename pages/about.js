75ca1d4814e985954a0548b3b5989db76e0dddcf
Danilo Ribeiro <danilribeiro@deloitte.com>
2023-04-10 10:13:46 -0300

    fix about scale

--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div className="flex flex-col gap-8 w-full justify-center items-center h-full transition-all hover:scale-110">
+    <div className="flex flex-col gap-8 w-full justify-center items-center h-full">
       <h1 className="text-4xl font-bold">About</h1>
-      <div className="bg-gradient-to-r from-gray-100 to-gray-300 justify-around h-96 shadow-xl rounded-2xl p-8 w-96 flex flex-col items-center border-8 border-gray-200">
+      <div className="bg-gradient-to-r from-gray-100 to-gray-300 justify-around h-96 shadow-xl rounded-2xl p-8 w-96 flex flex-col items-center border-8 border-gray-200 transition-all hover:scale-110">
         <Image
           src="/images/charizard.png"
           width={200}
